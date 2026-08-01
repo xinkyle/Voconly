fe6a38939877f8e404c63e985a33d340cd02a78e
xinkyle <savagexy23@126.com>
2026-08-01 19:28:29 +0800

    移除不需要的文件

--- build_script.ps1
+++
@@ -1,8 +0,0 @@
-# Build script for release
-# If you encounter path too long errors, either:
-# 1. Move project to a shorter path (e.g., C:\dev\Voconly)
-# 2. Enable Windows long path support: https://learn.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation
-
-$env:TRANSCRIBE_CMAKE_ARGS = "-DCMAKE_CXX_FLAGS=/utf-8"
-Set-Location "$PSScriptRoot\src-tauri"
-cargo build --release
